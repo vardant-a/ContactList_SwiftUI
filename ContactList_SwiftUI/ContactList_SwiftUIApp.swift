88a32e0ecb9c4c05205d9a144dfcaf21e0e74566
Aleksei Lokhin <lokhin.a.a@gmail.com>
2022-09-21 22:43:23 +0300

    Initial Commit

---
+++ ContactList_SwiftUI/ContactList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList_SwiftUIApp.swift
+//  ContactList_SwiftUI
+//
+//  Created by Алексей on 21.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
