7aaa6d5d384919f708d5d904e8fcd8fbbac3b0b8
Aleksei Lokhin <lokhin.a.a@gmail.com>
2022-09-23 22:55:15 +0300

    Добавлено изображение

--- ContactList_SwiftUI/Views/DetailView.swift
+++ ContactList_SwiftUI/Views/DetailView.swift
@@ -10,22 +10,24 @@ import SwiftUI
 struct DetailView: View {
     let person: Person!
     
+    private let height = 160.0
+    
     var body: some View {
         NavigationStack {
-            VStack {
+            Form {
                 Image("default")
-                    .frame(width: 125, height: 125)
-                    .padding(.top, 20)
-                Form {
-                    HStack {
-                        Image(systemName: "phone")
-                        Text(person.number)
-                    }
-                    
-                    HStack {
-                        Image(systemName: "tray")
-                        Text(person.email)
-                    }
+                    .resizable()
+                    .frame(width: height * 1.1, height: height)
+                    .cornerRadius(20)
+                
+                HStack {
+                    Image(systemName: "phone")
+                    Text(person.number)
+                }
+                
+                HStack {
+                    Image(systemName: "tray")
+                    Text(person.email)
                 }
             }
             .navigationTitle(person.fullName)
@@ -35,6 +37,6 @@ struct DetailView: View {
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(person: Person.init(id: 1, firstName: "test", lastName: "2", number: "1", email: "3"))
+        DetailView(person: Person.init(id: 1, firstName: "title", lastName: "last", number: "1", email: "3"))
     }
 }
